118d1e2a855eb1444bca95bc2ffb77708c1bf789
RoastedChestnut <chestnut.roasted@outlook.com>
2025-11-06 16:47:34 +0800

    chore(release): bump version to 0.0.4

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule HanaShirabe.Umbrella.MixProject do
   def project do
     [
       apps_path: "apps",
-      version: "0.0.3",
+      version: "0.0.4",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
