23a9ab0e8417b95e7380422652688d1f65ef3719
RoastedChestnut <chestnut.roasted@outlook.com>
2025-12-02 15:58:48 +0800

    feat: enhance theme handling and typography in content component

--- apps/hana_shirabe_web/lib/hana_shirabe_web/components/hana/content.ex
+++ apps/hana_shirabe_web/lib/hana_shirabe_web/components/hana/content.ex
@@ -4,13 +4,24 @@ defmodule HanaShirabeWeb.Content do
 
   # alias Phoenix.LiveView.JS
 
-  # TODO: implement options
-  # attr :other_class, :map
+  @doc """
+  """
+  attr :id, :string, default: ""
+  attr :theme, :atom, required: true, values: [:archaiologia, :minimalism, :non_ugc]
+  attr :other_class, :list, default: []
   slot :inner, required: true
 
   def render_inner(assigns) do
     ~H"""
-    <div class="prose"><% @inner |> raw() %></div>
+    <div data-theme={theme_class(@theme)}>
+      <div class={@other_class ++ "prose"} id={@id}>
+        <% @inner |> render_slot() %>
+      </div>
+    </div>
     """
   end
+
+  defp theme_class(:archaiologia), do: "archaiologia"
+  defp theme_class(:minimalism), do: "minimalism"
+  defp theme_class(:non_ugc), do: "non-ugc"
 end
