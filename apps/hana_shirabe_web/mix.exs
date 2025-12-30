d9e7402254c472e62fcdb2f3895d94a85d3dbb67
RoastedChestnut <chestnut.roasted@outlook.com>
2025-12-30 19:38:32 +0800

    Refactor code structure for improved readability and maintainability

--- apps/hana_shirabe_web/mix.exs
+++ apps/hana_shirabe_web/mix.exs
@@ -39,11 +39,11 @@ defmodule HanaShirabeWeb.MixProject do
   defp deps do
     [
       ## Phoenix 网络应用
-      {:phoenix, "~> 1.8.1"},
+      {:phoenix, "~> 1.8.3"},
       {:phoenix_ecto, "~> 4.5"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 1.1.0"},
+      {:phoenix_live_view, "~> 1.1.17"},
       {:lazy_html, ">= 0.1.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
